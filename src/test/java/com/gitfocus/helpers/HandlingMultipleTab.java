2db49e16c4904bd450d7cc415e22cdada3eae551
vibi <vibi.vijayakumar@gmail.com>
2023-11-29 15:48:26 +0530

    windowHandle and windowHandles

--- src/test/java/com/gitfocus/helpers/HandlingMultipleTab.java
+++ src/test/java/com/gitfocus/helpers/HandlingMultipleTab.java
@@ -1,6 +1,7 @@
 package com.gitfocus.helpers;
 
 import java.sql.Driver;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -30,19 +31,45 @@ public class HandlingMultipleTab
 		
 		driver.findElement(By.xpath("//input[@id='name']")).sendKeys("abcddd");
 		
-		String windowID = driver.getWindowHandle();
+		String pwindowID = driver.getWindowHandle();
 		
 		driver.findElement(By.xpath("//button[@id='newTabBtn']")).click();
 		
 		Set<String> windowIDs = driver.getWindowHandles();
+		//Method 1 : Iterator
+//		Iterator<String> it =windowIDs.iterator();
+//		String parentWin = it.next();
+//		String childWin = it.next();
 		
-		 Iterator<String> it  = windowIDs.iterator();
-		 String parentWindow = it.next();
-		 String childWindow =it.next();
-		 
-		 System.out.println(parentWindow);
-		 System.out.println(childWindow);
 		
+		//Method 2: Array List
+		ArrayList<String> windIDs = new ArrayList<String>(windowIDs);
+//		
+//		String pID = windIDs.get(0);
+//		String cID = windIDs.get(1);
+//		System.out.println(pID);
+//		System.out.println(cID);
+//		
+//		//Switching between tabs
+//		
+//		driver.switchTo().window(pID);
+//		System.out.println(driver.getTitle());
+//		driver.switchTo().window(cID);
+//		System.out.println(driver.getTitle());
+//		
+		
+		//Method 3 using for loop
+		
+		for(String wid:windIDs)
+		{
+			//System.out.println(driver.switchTo().window(wid).getTitle());
+			if(!wid.equals(pwindowID))
+			{
+				System.out.println("heii");
+				driver.switchTo().window(wid);
+				System.out.println(driver.switchTo().window(wid).getTitle());
+			}
+		}
 		
 	}
 }
